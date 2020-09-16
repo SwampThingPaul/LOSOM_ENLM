1c8bf009003eb468fc96d7c3fadc4f849b12a57a
Paul Julian <SwampThingPaul@users.noreply.github.com>
2020-09-16 08:58:19 -0400

    updates and changes

---
+++ makefile.R
@@ -0,0 +1,14 @@
+
+## Renders and moves files from repo to "/slides" on personal webpage
+library(AnalystHelper)
+
+# Render slides
+rmarkdown::render("Julian_LOSOM_ENLM.Rmd")
+
+list.files(full.names=T)
+files=c("./libs/","./resources/","./Julian_LOSOM_ENLM.html")
+
+# local webpage
+webpage.loc="c:/Julian_LaCie/_GitHub/SwampThingPaul.github.io/slides"
+Folder.Maker(paste0(webpage.loc,"/20200918_LOSOM_ENLM/"))
+file.copy(files,paste0(webpage.loc,"/20200918_LOSOM_ENLM"),overwrite=T,recursive=T)
